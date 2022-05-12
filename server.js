b68adaacf3faa55828a12fdb5dfd9d534fb71f95
charon <cariasjosue@gmail.com>
2022-05-12 19:49:37 -0400

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+app.listen(PORT, () => console.log(`Connected`));
