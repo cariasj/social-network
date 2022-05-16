dc64e7bb695fd9a514ad4ce607bace258de07d3b
charon <cariasjosue@gmail.com>
2022-05-16 11:22:31 -0400

    controllers and models

---
+++ controllers/thought-controller.js
@@ -0,0 +1,134 @@
+const { User, Thought} = require('../models');
+
+const thoughtController = {
+
+  // all thoughts
+  getAllThoughts(req,res) {
+    Thoughts.find({})
+    .populate({path: 'reactions', select: '-__v'})
+    .select('-__v')
+    .then(dbThoughtsData => res.json(dbThoughtsData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+
+  // thoughts by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(thoughts => {
+        if (!thoughts) {
+          res.status(404).json({ message: 'Invalid ID' });
+          return;
+        }
+        res.json(thoughts);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  createThought({ body }, res) {
+    Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(thoughts => {
+            if (!thoughts) {
+                res.status(404).json({ message: 'Invalid' });
+                return;
+            }
+            res.json(thoughts);
+        })
+        .catch(err => res.json(err));
+},
+
+  // update Thought by id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "Invalid" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // delete thought by ID
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(thoughts => {
+        if (!thoughts) {
+          res.status(404).json({ message: 'Invalid ID' });
+          return;
+        }
+        return User.findOneAndUpdate(
+          { _id: parmas.userId },
+          { $pull: { thoughts: params.Id } },
+          { new: true }
+        )
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'Invalid' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  createReaction({params, body}, res) {
+    Thought.findOneAndUpdate(
+      {_id: params.thoughtId}, 
+      {$push: {reactions: body}}, 
+      {new: true, runValidators: true})
+      .then(updatedThought => {
+        if (!updatedThought) {
+            res.status(404).json({
+                message: 'Invalid'
+            });
+            return;
+        }
+        res.json(updatedThought);
+    })
+    .catch(err => res.json(err));
+},
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then(thoughts => {
+        if (!thoughts) {
+          res.status(404).json({ message: 'error'});
+          return;
+        }
+       res.json(thoughts);
+      })
+      .catch(err => res.json(err));
+  }
+
+
+};
+
+module.exports = thoughtController
---
+++ controllers/user-controller.js
@@ -0,0 +1,97 @@
+
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUsers(req,res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+         })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        });
+      },
+
+  getUserById({ params }, res) {
+    Users.findOne({_id: params.id })
+    .populate({path: 'thoughts', select: '-__v'})
+    .populate({path: 'friends', select: '-__v'})
+    .select('-__v')
+    .then(dbUsersData => {
+        if(!dbUsersData) {
+            res.status(404).json({message: 'No results'});
+            return; 
+        }
+        res.json(dbUsersData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+},
+
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No results' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  deleteUser({ params }, res) {
+    Thought.deleteMany({ userId: params.id })
+      .then(() => {
+        User.findOneAndDelete({ userId: params.id })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No results' });
+              return;
+            }
+            res.json(dbUserData);
+          });
+      })
+      .catch(err => res.json(err));
+  },
+
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No results' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $pull: { friends: params.friendId }},
+        { new: true}
+    )
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.json(err));
+}
+};
+
+module.exports = userController
